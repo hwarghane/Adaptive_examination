1e83adecfe2e9dd91285cb4129e37c74a1a9dadd
homen w <co.2022.hvwarghane@bitwardha.ac.in>
2026-07-02 13:33:21 +0530

    Trigger Render redeploy - Atlas whitelist fix

--- backend/server.js
+++ backend/server.js
@@ -61,4 +61,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT} | env: ${process.env.NODE_ENV}`));
